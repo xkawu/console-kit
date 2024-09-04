63ff7b5e34af725a5a40a0ea56c3fd9c68f46542
xkawu <120594499+xkawu@users.noreply.github.com>
2024-09-04 19:45:17 +0200

    [ FIX ] - fixed dts files

--- dist/client.d.ts
+++ dist/client.d.ts
@@ -1,1 +1,37 @@
-declare module "@kawu/console-kit";
+/// <reference types="node" />
+import { EventEmitter } from "node:events";
+interface SelectOptions {
+    values: Array<string>;
+    defaultValueIndex?: number;
+    selectedText?: string;
+    unselectedText?: string;
+    cleanafter?: boolean;
+}
+export declare class ConsoleKit {
+    private _loaderInterval;
+    private _currentProgress;
+    comment(message: string, timestamp?: boolean): void;
+    info(message: string, timestamp?: boolean): void;
+    warn(message: string, timestamp?: boolean): void;
+    success(message: string, timestamp?: boolean): void;
+    error(message: string, timestamp?: boolean): void;
+    startLoading(message: string, timestamp?: boolean): void;
+    stopLoading(clearLine?: boolean): void;
+    startProgress(message: string, percentage?: number, timestamp?: boolean): void;
+    editProgress(percentage: number, message: string): void;
+    endProgress(clearLine?: boolean): void;
+    prompt(message: string, character?: string): Promise<string>;
+    yesno(message: string, defaultValue: boolean): Promise<boolean>;
+    select(selectOptions: SelectOptions): Promise<unknown>;
+}
+export declare class Debugger extends EventEmitter {
+    constructor();
+}
+interface DebuggerEvents {
+    debug: (message: string) => void;
+}
+export declare interface Debugger {
+    on<U extends keyof DebuggerEvents>(event: U, listener: DebuggerEvents[U]): this;
+    emit<U extends keyof DebuggerEvents>(event: U, ...args: Parameters<DebuggerEvents[U]>): boolean;
+}
+export { bold, italic, underline } from "chalk";
